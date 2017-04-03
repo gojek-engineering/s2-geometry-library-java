ac2d9710cfb59798d21d43ad15561ec4dc9f408f
aditya-sanghi <aditya@c42.in>
2017-04-03 12:44:52 +0700

    [Aditya] add folder creation for vendor in rake task and fix s2geometry.rb

--- rb/lib/s2geometry.rb
+++ rb/lib/s2geometry.rb
@@ -1,1 +1,6 @@
 require 's2geometry/version'
+require 's2geometry/s2_cell'
+
+require 's2geometry/vendor/s2-geometry-java.jar'
+require 's2geometry/vendor/jsr305.jar'
+require 's2geometry/vendor/guava-r09.jar'
--- rb/lib/s2geometry/s2_cell.rb
+++ rb/lib/s2geometry/s2_cell.rb
@@ -1,9 +1,5 @@
 module S2Geometry
   class S2Cell
-    require_relative '../vendor/s2-geometry-java.jar'
-    require_relative '../vendor/jsr305.jar'
-    require_relative '../vendor/guava-r09.jar'
-
     import 'com.google.common.geometry'
 
     S2_LEVEL_4_X_4_METRES = 21
