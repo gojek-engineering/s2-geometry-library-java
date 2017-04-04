922c2fdb00b83c10a5992401d08fc48cc84f22cc
Sidu Ponnappa <ckponnappa@gmail.com>
2017-04-04 23:35:08 +0800

    add spec for lat/long factory building cell at leaf level

--- rb/lib/s2geometry/s2_cell.rb
+++ rb/lib/s2geometry/s2_cell.rb
@@ -1,4 +1,6 @@
-module S2Geometry
+module S2Geometry #:nodoc:
+  # Convert latitude and longitude (floats or doubles) 
+  # to a S2Cell insteance at leaf node level (30)
   def S2Cell.build_from_lat_long(latitude, longitude)
     self.new(
       com.google.common.geometry.S2CellId.from_lat_lng(
--- rb/spec/s2geometry/s2_cell_spec.rb
+++ rb/spec/s2geometry/s2_cell_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe S2Geometry::S2Cell do
+  let(:latitude) { -6.2 }
+  let(:longitude) { 106.816667 }
+  let(:s2cell) { described_class.build_from_lat_long(latitude, longitude) }
+  
   context 'factory' do
     it 'can build from latitude, longitude' do
-      expect(described_class.build_from_lat_long(1, 1)).to be_an_instance_of(described_class)
+      expect(s2cell).to be_an_instance_of(described_class)
+    end
+    
+    it 'builds from latitude, longitude at leaf level' do
+      expect(s2cell.is_leaf).to eq(true)
     end
   end
 end
